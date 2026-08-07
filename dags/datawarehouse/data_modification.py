e3f4845980a914f04af6346283eed4ea89408ca4
Enpewww <nadyanputra10@gmail.com>
2026-08-07 15:04:13 +0700

    update data_loading, data_modification, data_transformation, data_utils, dwh script

---
+++ dags/datawarehouse/data_modification.py
@@ -0,0 +1,93 @@
+import logging
+
+logger = logging.getLogger(__name__)
+table = "yt_api"
+
+def insert_rows(conn, cur, schema, row):
+
+    try:
+        if schema == "staging":
+
+            video_id = 'video_id'
+
+            cur.execute(f""" INSERT INTO {schema}.{table} ("Video_Id", "Video_Title", "Upload_Date", "Duration", "Video_Views", "Likes_Count", "Comments_Count")
+            VALUES (%(video_id)s, %(title)s, %(published_at)s, %(duration)s, %(view_count)s, %(like_count)s, %(comment_count)s)
+            """, row 
+        )
+
+        else:
+            video_id = 'Video_id'
+
+            cur.execute(f""" INSERT INTO {schema}.{table} ("Video_Id", "Video_Title", "Upload_Date", "Duration", "Video_Type", "Video_Views", "Likes_Count", "Comments_Count")
+            VALUES (%(Video_Id)s, %(Video_Title)s, %(Upload_Date)s, %(Duration)s, %(Video_Type)s, %(Video_Views)s, %(Likes_Count)s, %(Comments_Count)s)
+            """, row
+        )
+
+        conn.commit()
+        logger.info(f"Inserted row into {schema}.{table}: {row[video_id]}")
+
+    except Exception as e:
+        logger.error(f"Error inserting row into {schema}.{table}: {row[video_id]}")
+        raise e
+
+def update_rows(curr, conn, schema, row):
+
+    try:
+        # Stagging
+        if schema == "staging":
+            video_id = 'video_id'
+            upload_date = 'published_at'
+            video_title = 'title'
+            video_views = 'view_count'
+            likes_count = 'like_count'
+            comments_count = 'comment_count'
+
+        # Core
+        else:
+            video_id = 'Video_id'
+            upload_date = 'Upload_Date'
+            video_title = 'Video_Title'
+            video_views = 'Video_Views'
+            likes_count = 'Likes_Count'
+            comments_count = 'Comments_Count'
+
+        curr.execute(
+            f"""
+            UPDATE {schema}.{table} 
+            SET "Video_Title" = %({video_title})s,
+                "Video_Views" = %({video_views})s,
+                "Likes_Count" = %({likes_count})s, 
+                "Comments_Count" = %({comments_count})s 
+            WHERE "Video_Id" = %({video_id})s AND "Upload_Date" = %({upload_date})s;
+            """, row
+
+        )
+
+        conn.commit()
+
+        logger.info(f"Updated row in {schema}.{table}: {row[video_id]}")
+
+    except Exception as e:
+        logger.error(f"Error updating row in {schema}.{table}: {row[video_id]} - {e}")
+        raise e
+
+def delete_rows(curr, conn, schema, ids_to_delete):
+
+    try:
+
+        ids_to_delete = f"""({', '.join(f"'{id}'" for id in ids_to_delete)})"""
+
+        curr.execute(
+            f"""
+            DELETE FROM {schema}.{table} 
+            WHERE "Video_Id" IN {ids_to_delete};
+            """
+        )
+
+        conn.commit()
+        logger.info(f"Deleted rows from {schema}.{table}: {ids_to_delete}")
+
+    except Exception as e:
+        logger.error(f"Error deleting rows from {schema}.{table}: {ids_to_delete} - {e}")
+        raise e
+
---
+++ dags/datawarehouse/dwh.py
@@ -0,0 +1,110 @@
+from datawarehouse.data_utils import get_conn_cursor, conn_curr_close, create_schema, create_table, get_video_ids
+from datawarehouse.data_loading import load_data
+from datawarehouse.data_modification import insert_rows, update_rows, delete_rows
+from datawarehouse.data_transformation import transform_data
+
+import logging
+from airflow.decorators import task
+
+logger = logging.getLogger(__name__)
+table = "yt_api"
+
+@task
+def staging_table():
+
+    schema = 'staging'
+
+    conn, curr = None, None
+
+    try:
+        conn, curr = get_conn_cursor()
+
+        YT_data = load_data()
+
+        create_schema(schema)
+        create_table(schema)
+
+        table_ids = get_video_ids(curr, schema)
+
+        for row in YT_data:
+
+            if len(table_ids) == 0:
+                insert_rows(conn, curr, schema, row)
+
+            else:
+                if row['video_id'] in table_ids:
+                    update_rows(curr, conn, schema, row)
+                else:
+                    insert_rows(conn, curr, schema, row)
+
+        ids_json = [row['video_id'] for row in YT_data]
+
+        ids_to_delete = set(table_ids) - ids_json
+
+        if ids_to_delete:
+            delete_rows(curr, conn, schema, ids_to_delete)
+
+        logger.info(f"Staging table '{schema}.{table}' updated successfully.")
+
+    except Exception as e:
+        logger.error(f"Error updating staging table '{schema}.{table}': {e}")
+        raise e
+
+    # Ensure connection and cursor are closed even if an error occurs
+    finally:
+        if conn and curr:
+            conn_curr_close(conn, curr)
+
+
+@task
+def core_table():
+
+    schema = 'core'
+
+    conn, curr = None, None
+
+    try:
+        conn, curr = get_conn_cursor()
+
+        create_schema(schema)
+        create_table(schema)
+
+        table_ids = get_video_ids(curr, schema)
+
+        current_video_ids = set()
+
+        curr.execute(f"""SELECT * FROM staging.{table};""")
+        rows = curr.fetchall()
+
+        for row in rows:
+
+            current_video_ids.add(row['Video_Id'])
+
+            if len(table_ids) == 0:
+                transformed_row = transform_data(row)
+                insert_rows(conn, curr, schema, transformed_row)
+
+            else:
+                transformed_row = transform_data(row)
+
+                if transformed_row['Video_Id'] in table_ids:
+                    update_rows(curr, conn, schema, transformed_row)
+
+                else:
+                    insert_rows(conn, curr, schema, transformed_row)
+
+        ids_to_delete = set(table_ids) - current_video_ids
+
+        if ids_to_delete:
+            delete_rows(curr, conn, schema, ids_to_delete)
+
+        logger.info(f"Staging table '{schema}.{table}' updated successfully.")
+
+    except Exception as e:
+        logger.error(f"Error updating staging table '{schema}.{table}': {e}")
+        raise e
+
+    finally:
+        # Ensure connection and cursor are closed even if an error occurs
+        if conn and curr:
+            conn_curr_close(conn, curr) 
