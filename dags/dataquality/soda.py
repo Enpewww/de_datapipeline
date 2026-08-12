7d625fd9474b881780332282bb6ea30c5b0cd679
Enpewww <nadyanputra10@gmail.com>
2026-08-12 17:43:38 +0700

    minor update

--- dags/dataquality/soda.py
+++ dags/dataquality/soda.py
@@ -3,15 +3,15 @@ from airflow.operators.bash import BashOperator
 
 logger = logging.getLogger(__name__)
 
-SODA_PATH = "/opt/airflow/dags/include/soda"
-DATA_SOURCE = "pg_datasource"
+SODA_PATH = "/opt/airflow/include/soda"
+DATASOURCE = "pg_datasource"
 
 
 def yt_elt_data_quality(schema):
     try:
         task =BashOperator(
             task_id = f"soda_test_{schema}",
-            bash_command = f"soda scan -d {DATA_SOURCE} -c {SODA_PATH}/configuration.yml -v SCHEMA={schema} {SODA_PATH}/{schema}_checks.yml",
+            bash_command=f"soda scan -d {DATASOURCE} -c {SODA_PATH}/configuration.yml -v SCHEMA={schema} {SODA_PATH}/{schema}_checks.yml",
         )
         return task
     
