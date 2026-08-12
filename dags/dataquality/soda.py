f14ea5105150d3dd4b388d1ee179bf78077e15ee
Enpewww <nadyanputra10@gmail.com>
2026-08-12 18:19:19 +0700

    update pytest

--- dags/dataquality/soda.py
+++ dags/dataquality/soda.py
@@ -9,7 +9,7 @@ DATASOURCE = "pg_datasource"
 
 def yt_elt_data_quality(schema):
     try:
-        task =BashOperator(
+        task = BashOperator(
             task_id = f"soda_test_{schema}",
             bash_command=f"soda scan -d {DATASOURCE} -c {SODA_PATH}/configuration.yml -v SCHEMA={schema} {SODA_PATH}/{schema}_checks.yml",
         )
