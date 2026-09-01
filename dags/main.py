5968d858274d927d5bae6dd116402dcb4654b6a9
Enpewww <nadyanputra10@gmail.com>
2026-09-01 17:16:58 +0700

    fixing

--- dags/main.py
+++ dags/main.py
@@ -47,7 +47,7 @@ with DAG(
     trigger_update_db = TriggerDagRunOperator(
         task_id="trigger_update_db",
         trigger_dag_id="update_db",
-        wait_for_completion=True,
+        wait_for_completion=False,
     )
 
     # Define task dependencies
@@ -69,7 +69,7 @@ with DAG(
     trigger_data_quality = TriggerDagRunOperator(
     task_id="trigger_data_quality",
     trigger_dag_id="data_quality",
-    wait_for_completion=True,
+    wait_for_completion=False,
     )
 
     # Define task dependencies
